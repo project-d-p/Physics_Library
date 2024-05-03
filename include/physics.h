d6dc724ebc87b563ea93345d39bf7f41ccbaa376
dkglee <ideade9@gmail.com>
2024-05-03 14:07:08 +0900

    FEAT : Header of the library for Users

---
+++ include/physics.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "../source/common/objects.h"
+#include "../source/common/object_info.h"
+#include "../source/gun/hitscan_shoot_process.h"
+#include "../source/gun/target_info.h"
+#include "../source/gun/source_info.h"
