809d21dc3b8881493ef6be1f90fe9e2d70d2b115
Hansangmin <ghtrm1037@gmail.com>
2024-05-04 20:41:22 +0900

    MERGE: temporary storage for merge

---
+++ include/Movement.h
@@ -0,0 +1,30 @@
+﻿#ifndef MOVEMENT_H
+#define MOVEMENT_H
+
+#include <memory>
+#include <PxPhysicsAPI.h>
+
+typedef struct {
+    double x;
+    double y;
+    double z;
+} t_movement;
+
+namespace Movement {
+class Movement {
+public:
+    Movement();
+
+    t_movement updatePosition();
+private:
+    std::shared_ptr<physx::PxScene> scene;
+    std::shared_ptr<physx::PxPhysics> physics;
+    std::shared_ptr<physx::PxController> character;
+    std::shared_ptr<physx::PxControllerManager> manager;
+
+    void initializeController();
+};
+
+}
+
+#endif
